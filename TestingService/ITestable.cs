892d0e485cacefea3f291328a6511b5697a023cf
Akash Lal <akashl@microsoft.com>
2019-09-04 16:57:35 +0530

    Init commit

---
+++ TestingService/ITestable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingService
+{
+    interface ITestable
+    {
+        void StartTask(int taskId);
+
+        void EndTask(int taskId);
+
+        void CreateResource(int resourceId);
+
+        void DeleteResource(int resourceId);
+
+        void BlockedOnResource(int resourceId);
+
+        void SignalUpdatedResource(int resourceId);
+
+        bool CreateNondetBool();
+
+        int CreateNondetInteger(int maxValue);
+    }
+}
