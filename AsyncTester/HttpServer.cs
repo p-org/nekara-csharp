666207ab52888958412df3b1ce602dbe6b429efc
Kumseok Jung <jungkumseok@gmail.com>
2019-09-18 14:24:11 +0530

    Wrote a blocking version of a basic http server and client.

---
+++ AsyncTester/HttpServer.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+
+namespace AsyncTester
+{
+    delegate void Middleware(Request request, Response response, Action next);
+
+    class Router
+    {
+        protected List<Middleware> middlewares;
+
+        public Router()
+        {
+            this.middlewares = new List<Middleware>();
+        }
+
+        public void Use(Middleware middleware)
+        {
+            this.middlewares.Add(middleware);
+        }
+
+        public Router Route(string path)
+        {
+            return new Router();
+        }
+    }
+
+    // light wrapper around the native HttpListenerRequest class to hide away the low-level details
+    class Request
+    {
+        private HttpListenerRequest request;
+
+        public Request(HttpListenerRequest request)
+        {
+            this.request = request;
+        }
+    }
+
+    // light wrapper around the native HttpListenerResponse class to hide away the low-level details
+    class Response
+    {
+        private HttpListenerResponse response;
+
+        public Response(HttpListenerResponse response)
+        {
+            this.response = response;
+        }
+
+        public void Send(int statusCode, string payload)
+        {
+            // Construct a response.
+            // string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(payload);
+
+            // Get a response stream and write the response to it.
+            this.response.ContentLength64 = buffer.Length;
+            System.IO.Stream output = this.response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            // You must close the output stream.
+            output.Close();
+            // listener.Stop();
+        }
+    }
+
+    // This is an HTTP Server providing an asynchronous API like Express.js
+    class HttpServer : Router
+    {
+        private string host;
+        private int port;
+        private HttpListener listener;
+
+        public HttpServer(string host, int port) : base()
+        {
+            this.host = host;
+            this.port = port;
+
+            this.listener = new HttpListener();
+            this.listener.Prefixes.Add("http://" + host + ":" + port.ToString() + "/");
+        }
+
+        public void Listen()
+        {
+            listener.Start();
+            Console.WriteLine("Listening...");
+
+            while (true)
+            {
+                HandleRequest();
+            }
+        }
+
+        private void HandleRequest()
+        {
+            HttpListenerContext context = listener.GetContext();
+
+            Request request = new Request(context.Request);
+            Response response = new Response(context.Response);
+            Action next = null;
+
+            int current = 0;
+            next = () => {
+                if (current < this.middlewares.Count)
+                {
+                    Middleware handler = this.middlewares[current];
+                    current++;
+                    handler(request, response, next);
+                }
+            };
+
+            next();
+        }
+    }
+}
--- AsyncTester/Tester.cs
+++ AsyncTester/Tester.cs
@@ -7,27 +7,34 @@ using System.Threading.Tasks;
 using System.Runtime.Remoting;
 using System.Runtime.Remoting.Channels;
 using System.Runtime.Remoting.Channels.Ipc;
-using System.Web;
+// using System.Web;
 using System.Net;
 using System.Net.Http;
 
 namespace AsyncTester
 {
-    public enum Mode { IPC, HTTP, TCP }  // AsyncTester could be used in a network setting. Easily switch between modes with this config flag.
+    // AsyncTester could be used in a network setting. Easily switch between transports with this config flag.
+    //   IPC - Inter-process communication (.net native)
+    //   HTTP - HyperText Transport Protocol
+    //   TCP - Raw TCP (Transmission Control Protocol)
+    //   WS - WebSocket (implemented over HTTP - this has a different communication pattern as the server-side can "push" to the client)
+    public enum Transport { IPC, HTTP, TCP, WS }
 
     public class TesterConfiguration
     {
-        private Mode _mode;
+        private Transport _transport;
+        private string host;    // used if Transport == HTTP
+        private int port;       // used if Transport == HTTP or TCP
 
         public TesterConfiguration()
         {
-            this._mode = Mode.IPC;
+            this._transport = Transport.HTTP;
         }
 
-        public Mode mode
+        public Transport transport
         {
-            get { return this._mode; }
-            set { this._mode = value; }
+            get { return this._transport; }
+            set { this._transport = value; }
         }
     }
 
@@ -42,8 +49,8 @@ namespace AsyncTester
             // Initialize a test runtime
             Runtime runtime = new Runtime(new RuntimeConfiguation());
 
-            // Depending on the mode, create the appropriate communication interface
-            if (this.config.mode == Mode.IPC)
+            // Depending on the transport, create the appropriate communication interface
+            if (this.config.transport == Transport.IPC)
             {
                 // Create and register the IPC channel
                 IpcServerChannel channel = new IpcServerChannel("tester");
@@ -54,11 +61,33 @@ namespace AsyncTester
 
                 // Wait for calls
                 Console.WriteLine("... Tester Server Listening on IPC Channel: " + channel.GetChannelUri());
-                
+            }
+            else if (this.config.transport == Transport.HTTP)
+            {
+                // Create an HttpServer and bind to network socket
+                HttpServer server = new HttpServer("localhost", 8080);
+
+                // Expose the service
+                server.Use((Request request, Response response, Action next) => {
+                    Console.WriteLine("Just Received a Request!");
+                    next();
+                });
+
+                server.Use((Request request, Response response, Action next) => {
+                    response.Send(200, "<HTML><BODY> Hello world!</BODY></HTML>");
+                    next();
+                });
+
+                server.Listen();
+
+                // Wait for calls
+                Console.WriteLine("... Tester Server Listening on HTTP Port: 8080");
             }
         }
     }
 
+    // This is the service object exposed to the client, and hosted on the server-side
+    // The API should be defined on this object.
     class TesterService : MarshalByRefObject
     {
         private int count = 0;
@@ -84,6 +113,38 @@ namespace AsyncTester
         }
     }
 
+    // This is the client-side proxy of the tester service.
+    // Used when the system is operating in a network setting.
+    class TesterServiceProxy
+    {
+        public Task<Object> Request(HttpClient client)
+        {
+            var tcs = new TaskCompletionSource<Object>();
+            // Call asynchronous network methods in a try/catch block to handle exceptions.
+            try
+            {
+                HttpResponseMessage response = client.GetAsync("http://localhost:8080").Result;
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = response.Content.ReadAsStringAsync().Result;
+
+                // Above three lines can be replaced with new helper method below
+                // string responseBody = await client.GetStringAsync(uri);
+
+                Console.WriteLine(responseBody);
+
+                tcs.SetResult(responseBody);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+
+            return tcs.Task;
+        }
+    }
+
     public class TesterClient
     {
         private TesterConfiguration config;
@@ -92,8 +153,8 @@ namespace AsyncTester
         {
             this.config = config;
 
-            // Depending on the mode, create the appropriate communication interface
-            if (this.config.mode == Mode.IPC)
+            // Depending on the transport, create the appropriate communication interface
+            if (this.config.transport == Transport.IPC)
             {
                 // Create and register the IPC channel
                 IpcClientChannel channel = new IpcClientChannel();
@@ -121,6 +182,35 @@ namespace AsyncTester
                     }
                     Console.WriteLine("Using seed: " + seed.ToString());
                     service.GetResult(seed);
+                    // Console.WriteLine(service.Count);
+                }
+            }
+            else if (this.config.transport == Transport.HTTP)
+            {
+                // Create and register the IPC channel
+                HttpClient client = new HttpClient();
+
+                // Create the proxy object -- an interface to the service
+                TesterServiceProxy service = new TesterServiceProxy();
+
+                while (true)
+                {
+                    Console.WriteLine("Next Seed? ");
+                    string input = Console.ReadLine();
+                    if (input == "q") break;
+
+                    int seed;
+
+                    try
+                    {
+                        seed = Int32.Parse(input);
+                    }
+                    catch (System.FormatException e)
+                    {
+                        seed = 0;
+                    }
+                    Console.WriteLine("Using seed: " + seed.ToString());
+                    service.Request(client);
                     // Console.WriteLine(service.Count);
                 }
             }
